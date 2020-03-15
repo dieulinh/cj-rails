fef2e5ba579e9f04bb133bc9308ecb75b61a7fe5
Linh Nguyen <linhvn09@gmail.com>
2020-03-16 00:00:48 +0700

    api enpoints for currency tickers

--- app/api/v1/currency_tickers.rb
+++ app/api/v1/currency_tickers.rb
@@ -5,16 +5,35 @@ module V1
     version 'v1', using: :path
     helpers Shared::JsonResponse
     helpers Shared::GetCoins
+
     resource :currency_tickers do
-      desc 'List currencies endpoint'
+      desc 'List latest prices endpoint'
+      params do
+        optional :currency_list, type: String
+      end
+      get '/' do
+        coin_list = params[:currency_list].present?&&params[:currency_list].split('-')
+        coin_list = ['ETHAUD','BTCAUD'] unless coin_list
+        currencies = CurrencyTicker.order('created_at DESC').limit(coin_list.length)
+        present currencies
+      end
+
+      desc 'Capture currencies endpoint'
       params do
         optional :currency_list, type: String
       end
       post '/' do
-        coin_list = params[:currency_list].present?&&params[:currency_list].split('-')
+        coin_list = params[:currency_list].present? && params[:currency_list].split('-')
         coin_list = ['ETHAUD','BTCAUD'] unless coin_list
         CaptureCoins.new(coin_list).call
-        ok!(201)
+        currencies = CurrencyTicker.order('created_at DESC').limit(coin_list.length)
+        present currencies, status: 201
+      end
+
+      desc 'History currency endpoint'
+      get '/:currency_name' do
+        currency_name = params[:currency_name]
+        present CurrencyTicker.where(currency_name: currency_name).order('created_at DESC')
       end
     end
   end
--- spec/requests/currency_tickers_spec.rb
+++ spec/requests/currency_tickers_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 describe V1::CurrencyTickers, type: :request do
   describe 'post /api/v1/currency_tickers' do
-    let(:currency_1) { create(:currency_ticker) }
-    let(:currency_2) { create(:currency_ticker) }
     let(:params) { { currency_list: 'ETHAUD' } }
     subject { post('/api/v1/currency_tickers', params: params) }
     it 'calls capture coins service' do
@@ -12,6 +10,26 @@ describe V1::CurrencyTickers, type: :request do
     it 'returns code 201' do
       subject
       expect(response.code).to eq('201')
+    end
+  end
+  describe 'get /api/v1/currency_tickers' do
+    let(:currency_1) { create(:currency_ticker, currency_name: 'ETHAUD') }
+    let(:currency_2) { create(:currency_ticker) }
+    let(:params) { { currency_list: 'ETHAUD' } }
+    before do
+      currency_1
+      currency_2
+    end
+    subject { get('/api/v1/currency_tickers', params: params) }
+    it 'calls capture coins service' do
+      subject
+      expect(JSON.parse(response.body)[0]['ask']).to eq(currency_1.ask)
+      expect(JSON.parse(response.body)[0]['bid']).to eq(currency_1.bid)
+      expect(JSON.parse(response.body)[0]['last']).to eq(currency_1.last)
+    end
+    it 'returns code 200' do
+      subject
+      expect(response.code).to eq('200')
     end
   end
 end
